e66f4033ead31695f8c7428b1526826e8c3d6816
Jungxx <jrx1102@gmail.com>
2019-06-24 22:39:52 +0800

    31进制流水号转换，不包含0，不进位
    31进制，不包含0的时候，进行流水号转换程式。

---
+++ 31COVER/31COVER/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox_TEN = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox_OTHER = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox_START = New System.Windows.Forms.TextBox()
+        Me.Button_COVER = New System.Windows.Forms.Button()
+        Me.TextBox_VALUE = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button_RUN = New System.Windows.Forms.Button()
+        Me.ListBox_ID = New System.Windows.Forms.ListBox()
+        Me.startCount = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.endCount = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox_TEN
+        '
+        Me.TextBox_TEN.Location = New System.Drawing.Point(90, 26)
+        Me.TextBox_TEN.Name = "TextBox_TEN"
+        Me.TextBox_TEN.Size = New System.Drawing.Size(258, 21)
+        Me.TextBox_TEN.TabIndex = 0
+        Me.TextBox_TEN.Text = "985"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(31, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "十进制"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 12)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "其他进制"
+        '
+        'TextBox_OTHER
+        '
+        Me.TextBox_OTHER.Location = New System.Drawing.Point(90, 57)
+        Me.TextBox_OTHER.Name = "TextBox_OTHER"
+        Me.TextBox_OTHER.Size = New System.Drawing.Size(258, 21)
+        Me.TextBox_OTHER.TabIndex = 2
+        Me.TextBox_OTHER.Text = "123456789ABCDEFGHJKLMNPRSTUWXYZ"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(31, 94)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 12)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "启始值"
+        '
+        'TextBox_START
+        '
+        Me.TextBox_START.Location = New System.Drawing.Point(90, 91)
+        Me.TextBox_START.Name = "TextBox_START"
+        Me.TextBox_START.Size = New System.Drawing.Size(258, 21)
+        Me.TextBox_START.TabIndex = 4
+        Me.TextBox_START.Text = "111"
+        '
+        'Button_COVER
+        '
+        Me.Button_COVER.Location = New System.Drawing.Point(273, 122)
+        Me.Button_COVER.Name = "Button_COVER"
+        Me.Button_COVER.Size = New System.Drawing.Size(75, 23)
+        Me.Button_COVER.TabIndex = 6
+        Me.Button_COVER.Text = "转换"
+        Me.Button_COVER.UseVisualStyleBackColor = True
+        '
+        'TextBox_VALUE
+        '
+        Me.TextBox_VALUE.Location = New System.Drawing.Point(90, 161)
+        Me.TextBox_VALUE.Name = "TextBox_VALUE"
+        Me.TextBox_VALUE.Size = New System.Drawing.Size(258, 21)
+        Me.TextBox_VALUE.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(31, 164)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 12)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "转换值"
+        '
+        'Button_RUN
+        '
+        Me.Button_RUN.Location = New System.Drawing.Point(609, 24)
+        Me.Button_RUN.Name = "Button_RUN"
+        Me.Button_RUN.Size = New System.Drawing.Size(82, 23)
+        Me.Button_RUN.TabIndex = 9
+        Me.Button_RUN.Text = "RunAll"
+        Me.Button_RUN.UseVisualStyleBackColor = True
+        '
+        'ListBox_ID
+        '
+        Me.ListBox_ID.FormattingEnabled = True
+        Me.ListBox_ID.ItemHeight = 12
+        Me.ListBox_ID.Location = New System.Drawing.Point(355, 60)
+        Me.ListBox_ID.Name = "ListBox_ID"
+        Me.ListBox_ID.Size = New System.Drawing.Size(336, 148)
+        Me.ListBox_ID.TabIndex = 10
+        '
+        'startCount
+        '
+        Me.startCount.Location = New System.Drawing.Point(393, 26)
+        Me.startCount.Name = "startCount"
+        Me.startCount.Size = New System.Drawing.Size(80, 21)
+        Me.startCount.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(354, 29)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(29, 12)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "开始"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(484, 29)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(29, 12)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "结束"
+        '
+        'endCount
+        '
+        Me.endCount.Location = New System.Drawing.Point(523, 26)
+        Me.endCount.Name = "endCount"
+        Me.endCount.Size = New System.Drawing.Size(80, 21)
+        Me.endCount.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(703, 217)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.endCount)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.startCount)
+        Me.Controls.Add(Me.ListBox_ID)
+        Me.Controls.Add(Me.Button_RUN)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox_VALUE)
+        Me.Controls.Add(Me.Button_COVER)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox_START)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox_OTHER)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox_TEN)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox_TEN As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox_OTHER As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox_START As System.Windows.Forms.TextBox
+    Friend WithEvents Button_COVER As System.Windows.Forms.Button
+    Friend WithEvents TextBox_VALUE As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button_RUN As System.Windows.Forms.Button
+    Friend WithEvents ListBox_ID As System.Windows.Forms.ListBox
+    Friend WithEvents startCount As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents endCount As System.Windows.Forms.TextBox
+
+End Class
---
+++ 31COVER/31COVER/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub Button_COVER_Click(sender As System.Object, e As System.EventArgs) Handles Button_COVER.Click
+        Dim res As String = Cover2(TextBox_TEN.Text, TextBox_OTHER.Text, TextBox_START.Text)
+        TextBox_VALUE.Text = res.Trim
+    End Sub
+
+
+
+    Private Function Cover2(num As Integer, rexStr As String, initstr As String) As String
+        Dim res As String = ""
+        'num   1
+        'rexStr 123456789ABCDEFGHJKLMNPRSTUWXYZ
+        'initstr 111
+        '默认31进制是从111开始，换算乘10进制，则是1*31*31 + 1* 31 + 1 = 993 开始属于第一位，则num = num + 992，即传递进来的值应加上992后再做转换
+        Dim count As Integer = rexStr.Length   '获得位数，31位
+        'Dim box As Char() = {"1", "1", "1"}  '初始化容器数组,编码后传入的1为111，初始化用于位数不够时补全
+        Dim box As Char() = {"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"}
+        Dim charIndex As Integer = box.Length - 1   'box的索引位置，默认从最后开始，不停的进位
+        num = num + 992
+        If num = 992 Then
+            MsgBox("传递进来的值不可为0")
+            res = "ERROR:传递进来的值不可为0"
+            Return res
+        End If
+        If num > 31 * 31 * 31 + 31 * 31 + 31 Then
+            MsgBox("传递进来的值已超出流水号位数")
+            res = "ERROR:传递进来的值已超出流水号位数"
+            Return res
+        End If
+        While num \ count <> 0
+            If num Mod count = 0 Then '整除时不进位
+                box(charIndex) = rexStr.Chars(rexStr.Length - 1) '进制中的最后一位
+                charIndex = charIndex - 1
+                num = num \ count - 1 '将要进位的值退一位回来
+            Else
+                box(charIndex) = rexStr.Chars((num Mod count) - 1)
+                charIndex = charIndex - 1
+                num = num \ count
+            End If
+        End While
+        If num > 0 Then
+            box(charIndex) = rexStr.Chars((num Mod count) - 1)
+        End If
+        res = New String(box, charIndex, box.Length - charIndex)
+        Return res
+    End Function
+
+    Private Sub Button_RUN_Click(sender As System.Object, e As System.EventArgs) Handles Button_RUN.Click
+        ListBox_ID.Items.Clear()
+        Dim a As Integer = startCount.Text
+        Dim b As Integer = endCount.Text
+        Dim rexStr As String = TextBox_OTHER.Text
+        Dim testTable As Hashtable = New Hashtable
+        For i = a To b
+            ListBox_ID.Items.Add(Cover2(i, rexStr, ""))
+            Try
+                testTable.Add(Cover2(i, rexStr, ""), "")
+            Catch ex As Exception
+                MsgBox("传递进来的值有误")
+                Exit Sub
+            End Try
+
+        Next
+    End Sub
+End Class
